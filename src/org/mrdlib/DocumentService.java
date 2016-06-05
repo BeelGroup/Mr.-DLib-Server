f0718ed71c12a9074bb1bddc8aa2d10c93f439fe
MillahMau <sophie_siebert@gmx.de>
2016-06-05 22:06:54 +0900

    Make GET Article dynamic
    implement database package, which connects to database and find entry with
    given id (#8)
    implement constant class which loads data from config file in resources (see example.properties)
    implement short error code if Data Entry is not available

--- src/org/mrdlib/DocumentService.java
+++ src/org/mrdlib/DocumentService.java
@@ -1,14 +1,16 @@
 package org.mrdlib;
 
-import java.util.List;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-//set Path and allow numbers, letters and -_,. . Save Path as document_id
-@Path("{document_id : [a-zA-Z0-9-_.,]+}")
+import org.mrdlib.database.DBConnection;
+import org.mrdlib.database.NoEntryException;
+
+//set Path and allow numbers, letters and -_.,  Save Path as document_id
+@Path("{documentId : [a-zA-Z0-9-_.,]+}")
 public class DocumentService {
 
 	DocumentExamples documentExample = new DocumentExamples();
@@ -17,7 +19,12 @@ public class DocumentService {
 	//set end of Path
 	@Path("/related_documents")
 	@Produces(MediaType.APPLICATION_XML)
-	public List<Related_article> getDocuments() {
-		return documentExample.getAllDocuments();
+	public DocumentSet getDocumentSet(@PathParam("documentId") String documentId) throws Exception {
+		DocumentSet documentset = DBConnection.getDocumentSetByOriginalId(documentId);
+		if(documentset.getSize() == 0)
+			throw new NoEntryException();
+		else
+			return documentset;
+		//return documentExample.getDocumentSet();
 	}
 }
