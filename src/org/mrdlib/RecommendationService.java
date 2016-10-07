97a34cb6ee020f913039cab269734a4d4ae68da7
sid-dinesh94 <f2012519@goa.bits-pilani.ac.in>
2016-10-08 00:37:53 +0900

    removed unecessary import statements

--- src/org/mrdlib/RecommendationService.java
+++ src/org/mrdlib/RecommendationService.java
@@ -1,12 +1,10 @@
 package org.mrdlib;
 
 import java.net.URI;
-import java.sql.SQLException;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
