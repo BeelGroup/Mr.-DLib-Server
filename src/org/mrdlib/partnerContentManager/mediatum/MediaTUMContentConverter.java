beb28e130ae982603a49eee57c96daac514273a6
wuestehube <wuestehube@10.211.55.4>
2017-04-05 21:43:44 +0200

    adds method stubs for convertion of mediaTUM to MDL data format

--- src/org/mrdlib/partnerContentManager/mediatum/MediaTUMContentConverter.java
+++ src/org/mrdlib/partnerContentManager/mediatum/MediaTUMContentConverter.java
@@ -2,6 +2,8 @@ package org.mrdlib.partnerContentManager.mediatum;
 
 import java.util.List;
 
+import org.mrdlib.partnerContentManager.mediatum.MDLContent.Document;
+
 /**
  * Implementation of ContentConverter for partner mediaTUM.
  * mediaTUM offers a standardized OAI interface exhibiting data in the OAI Dublin Core format (http://www.openarchives.org/OAI/openarchivesprotocol.html).
@@ -16,5 +18,50 @@ public class MediaTUMContentConverter implements IContentConverter<OAIDCRecordCo
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	private Document MapMediaTumToDocumentTable() {
+		// TODO
+		return null;
+	}
+	
+	private Document MapMediaTumToDocumentAbstractTable() {
+		// TODO
+		return null;
+	}
 
+	private Document MapMediaTumToDocumentExternalIdTable() {
+		// TODO
+		return null;
+	}
+	
+	private Document MapMediaTumToDocumentKeyphraseTable() {
+		// TODO
+		return null;
+	}
+	
+	private Document MapMediaTumToDocumentKeyphraseCountTable() {
+		// TODO
+		return null;
+	}
+	
+	private Document MapMediaTumToDocumentPersonTable() {
+		// TODO
+		return null;
+	}
+	
+	private Document MapMediaTumToDocumentTitleSearchesTable() {
+		// TODO
+		return null;
+	}
+	
+	private Document MapMediaTumToDocumentTranslatedFieldTable() {
+		// TODO
+		return null;
+	}
+	
+	private Document MapMediaTumToPersonTable() {
+		// TODO
+		return null;
+	}
+	
 }
