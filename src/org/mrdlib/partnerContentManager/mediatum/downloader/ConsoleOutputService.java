7968df1d351a2719911d9ded37ffc033f0b02672
wuestehube <wuestehube@10.211.55.4>
2017-04-26 18:02:51 +0200

    Adds current status of downloader.

---
+++ src/org/mrdlib/partnerContentManager/mediatum/downloader/ConsoleOutputService.java
@@ -0,0 +1,42 @@
+package org.mrdlib.partnerContentManager.mediatum.downloader;
+
+import java.util.Arrays;
+
+/**
+ * Provides helper functions for outputting text on the console.
+ */
+public class ConsoleOutputService {
+
+    /**
+     * Prints out error. Does not halt program execution.
+     * @param message Message that describes the occurred error.
+     * @param e Exception that occurred.
+     */
+    public static void printOutError(String message, Exception e) {
+        System.err.println(message);
+        if (e != null) {
+            //noinspection ThrowablePrintedToSystemOut
+            System.err.println(e);
+        }
+        System.err.println(Arrays.toString(Thread.currentThread().getStackTrace()));
+    }
+
+    /**
+     * Prints out error. Does not halt program execution.
+     * @param message Message that describes the occurred error.
+     */
+    public static void printOutError(String message) {
+        System.err.println(message);
+        System.err.println(Arrays.toString(Thread.currentThread().getStackTrace()));
+    }
+
+    /**
+     * Prints out status messages to the console. If possible include measurements to indicate progress in the passed
+     * status.
+     * @param status Status to print out.
+     */
+    public static void printOutStatus(String status) {
+        System.out.println(status);
+    }
+
+}
---
+++ src/org/mrdlib/partnerContentManager/mediatum/downloader/ExplorationState.java
@@ -0,0 +1,10 @@
+package org.mrdlib.partnerContentManager.mediatum.downloader;
+
+/**
+ * Defines constants for marking crawled elements as either already explore or not (yet) explored.
+ */
+public enum ExplorationState {
+
+    EXPLORED, NOT_EXPLORED
+
+}
