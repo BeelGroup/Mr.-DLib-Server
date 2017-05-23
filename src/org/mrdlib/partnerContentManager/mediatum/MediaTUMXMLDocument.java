e2b1917f67f1d2b934a7644d074da13babfe22c7
Max Wuestehube <max.wuestehube@gmail.com>
2017-05-23 20:30:21 +0200

    improves comments of MediaTUMXMLDocument

--- src/org/mrdlib/partnerContentManager/mediatum/MediaTUMXMLDocument.java
+++ src/org/mrdlib/partnerContentManager/mediatum/MediaTUMXMLDocument.java
@@ -20,20 +20,24 @@ import org.mrdlib.partnerContentManager.gesis.Tuple;
 import org.mrdlib.partnerContentManager.gesis.XMLDocument;
 
 /**
- * 
- * 
- * @author Millah
+ * @author Millah, Wuestehube
  *
- * stores and preprocesses all necessary information of an (academic) document
- *
+ * Holds information of an (academic) document.
+ * This class acts as an intermediate between partner content module and database.
  */
 public class MediaTUMXMLDocument extends XMLDocument {
+	
+	// just for debugging purposes
+	// TODO: remove documentPath and id
 	private String documentPath;
 	private String id;
+	
+	// attributes of the document
 	private String title;
 	private String fulltitle;
 	private String cleantitle;
 	private String language;
+	// stored as a list since abstracts in multiple languages may be available
 	private ArrayList<Abstract> abstr = new ArrayList<Abstract>();
 	private int year;
 	private int facetYear;
@@ -46,30 +50,50 @@ public class MediaTUMXMLDocument extends XMLDocument {
 	private String license;
 	private String fullText;
 	
+	// a mapping construct is used in the storing process
 	private LinkedHashSet<Person> authors = new LinkedHashSet<Person>();
 	private Map<String, String> typeMap = new HashMap<String, String>();
 	private Map<String, String> languageMap = new HashMap<String, String>();
 	private Map<Tuple, String> typeResolveMap = new HashMap<Tuple, String>();
 
 	/**
-	 * maps a string to a specific (in a config file determined) enum
+	 * Constructor, which initialize the needed Maps for mapping and resolving types and language.
+	 * Set some default values.
+	 * 
+	 * @param typeMap
+	 * @param languageMap
+	 * @param typeResolveMap
+	 */
+	public MediaTUMXMLDocument(Map<String, String> typeMap, Map<String, String> languageMap,
+			Map<Tuple, String> typeResolveMap) {
+		super(typeMap, languageMap, typeResolveMap);
+		
+		this.typeMap = typeMap;
+		this.languageMap = languageMap;
+		this.typeResolveMap = typeResolveMap;
+		type = "unknown";
+		publishedInRank = 10;
+	}
+	
+	/**
+	 * Maps a string to a specific (in a config file determined) enum.
 	 * 
 	 * @param original string of the type
 	 * @return enum type
 	 */
 	private String chooseType(String type) {
 		String tempType;
-		//if empty its unknown
+		// if empty its unknown
 		if (type.equals(""))
 			return "unknown";
 		else {
-			//get the corresponding enum type
+			// get the corresponding enum type
 			tempType = typeMap.get(type.toLowerCase());
-			//if this is not null (-> defined), return the new type
+			// if this is not null (-> defined), return the new type
 			if (tempType != null && !tempType.equals(""))
 				return tempType;
 			else {
-				//otherwise say its not handled yet but return unknown
+				// otherwise say its not handled yet but return unknown
 				System.out.println("In Export " + documentPath + " In Document: " + id + ": Undefined Type: " + type);
 				return "unknown";
 			}
@@ -77,33 +101,33 @@ public class MediaTUMXMLDocument extends XMLDocument {
 	}
 
 	/**
-	 * since multiple types can be set, select on of these which seems to be most reasonable
+	 * Since multiple types can be set, select on of these which seems to be most reasonable.
 	 */
 	private void selectType() {
 		Iterator<String> it;
 		String allTypes = "";
 		String current;
 
-		//if no type is set, its unknown
+		// if no type is set, its unknown
 		if (typeSet.isEmpty())
 			type = "unknown";
 		else {
 			it = typeSet.iterator();
-			//for each occuring type
+			// for each occuring type
 			while (it.hasNext()) {
 				current = it.next();
-				//save every occuring type for troubleshooting
+				// save every occuring type for troubleshooting
 				allTypes = allTypes + ", " + current.toString();
-				//if until now there is no "good" type, set current type
+				// if until now there is no "good" type, set current type
 				if (type == null || type.equals("unknown"))
 					type = current.toString();
-				//if there is another good or reasonable type, ask the resolveMap how to handle the conflict
+				// if there is another good or reasonable type, ask the resolveMap how to handle the conflict
 				else if (!type.equals(current.toString())) {
 					Tuple tupleKey = new Tuple(type, current.toString());
-					//if the resolve map has an answer, set it as solution
+					// if the resolve map has an answer, set it as solution
 					if (typeResolveMap.containsKey(tupleKey))
 						type = typeResolveMap.get(tupleKey);
-					//if not, say its not handled, but set unknown
+					// if not, say its not handled, but set unknown
 					else {
 						System.out.println(
 								"In Export " + documentPath + " In Document: " + id + ": Multiple Types: " + allTypes);
@@ -115,7 +139,9 @@ public class MediaTUMXMLDocument extends XMLDocument {
 	}
 
 	/**
-	 * @return the authors as a String, single names seperated by ","
+	 * Iterates over the authors of the document and returns them as a string.
+	 * 
+	 * @return the authors as a String, single names separated by ","
 	 */
 	public String getAuthorsAsString() {
 		String authorsString = "";
@@ -130,22 +156,23 @@ public class MediaTUMXMLDocument extends XMLDocument {
 	}
 
 	/**
-	 * add an author to the document, but preprocess it before
-	 * @param author, which is to be added
+	 * Adds an author to the document and preprocesses it before.
+	 * 
+	 * @param the author to add
 	 */
 	
 	public void addAuthor(String author) {
 		Person person;
 		
-		//is the person is completely written in upper case letters, capitalize it
+		// if the person is completely written in upper case letters, capitalize it
 		if (author.matches("[^a-z]*")) {
 			author = WordUtils.capitalizeFully(author);
-			//if the authors starts with "." dismiss it
+			// if the authors starts with "." dismiss it
 			if (author.startsWith("."))
 				author = author.substring(1);
 			char[] authorChar = author.toCharArray();
 			
-			//if the author contains a "." write the character followd by it in upper case
+			// if the author contains a "." write the character followd by it in upper case
 			for (int i = 0; i < authorChar.length; i++) {
 				if (authorChar[i] == '.') {
 					authorChar[i - 1] = Character.toUpperCase(authorChar[i - 1]);
@@ -154,23 +181,23 @@ public class MediaTUMXMLDocument extends XMLDocument {
 			author = String.valueOf(authorChar);
 		}
 		
-		//if the author is not in a acceptable format, the whole string is written to unstructured
+		// if the author is not in a acceptable format, the whole string is written to unstructured
 		if (!author.contains(",") || author.equals("[Unknown]") || author.endsWith(","))
 			person = new Person(author);
-		//otherwise split up in firstname, middlename, surname
+		// otherwise split up in first name, middle name, surname
 		else {
-			//surname is the first name until first ","
+			// surname is the first name until first ","
 			String surname = author.substring(0, author.indexOf(","));
 			
-			//firstname is everything behind
+			// first name is everything behind
 			String firstname = author.substring(author.indexOf(",") + 2);
 			
-			//if the firstname contains a " " it has a middlename, extract it
+			// if the first name contains a " " it has a middle name, extract it
 			if (firstname.contains(" ")) {
 				String middlename = firstname.substring(firstname.indexOf(" ") + 1);
 				firstname = firstname.substring(0, firstname.indexOf(" "));
 				person = new Person(firstname, middlename, surname);
-			//a middlename to extract is also present if there are points in the first name
+			// a middle name to extract is also present if there are points in the first name
 			} else if (firstname.matches("[A-Z].[A-Z].")) {
 				String middlename = firstname.substring(firstname.indexOf(".") + 1);
 				firstname = firstname.substring(0, firstname.indexOf(".") + 1);
@@ -179,12 +206,12 @@ public class MediaTUMXMLDocument extends XMLDocument {
 				person = new Person(firstname, surname);
 			}
 		}
-		//add the person to the authors
+		// add the person to the authors
 		authors.add(person);
 	}
 
 	/**
-	 * call the normalizing functions in the necessary order
+	 * Call the normalizing functions in the necessary order.
 	 */
 	public void normalize() {
 		selectTitle();
@@ -199,7 +226,7 @@ public class MediaTUMXMLDocument extends XMLDocument {
 	}
 	
 	/**
-	 * normalize publishedIn by cutting of "; ..."
+	 * Normalize publishedIn by cutting of "; ...".
 	 */
 	private void normalizePublishedIn() {
 		if(publishedIn != null)
@@ -208,7 +235,7 @@ public class MediaTUMXMLDocument extends XMLDocument {
 	}
 
 	/**
-	 * normalize Title by capitalizing it, if completly uppercase and erase " " if " :"
+	 * Normalize Title by capitalizing it, if completly uppercase and erase " " if " :".
 	 */
 	private void normalizeTitle() {
 		if (title.matches("[^a-z]*"))
@@ -218,28 +245,28 @@ public class MediaTUMXMLDocument extends XMLDocument {
 	}
 
 	/**
-	 * preprocess keywords, by deleting title from it, putting everything in lower case, and splitting keywords at ":" and ","
+	 * Preprocesses keywords, by deleting title from it, putting everything in lower case, and splitting keywords at ":" and ",".
 	 */
 	private void tidyUpKeywords() {
-		//remove title
+		// remove title
 		keywords.remove(title);
 
 		String[] copy = new String[keywords.size()];
 		keywords.toArray(copy);
 
 		for (String keyword : copy) {
-			//put in lowercase
+			// put in lowercase
 			String lowerKeyword = keyword.toLowerCase();
 			keywords.remove(keyword);
 			keywords.add(lowerKeyword);
 
-			//split at ":"
+			// split at ":"
 			if (lowerKeyword.contains(":")) {
 				keywords.remove(lowerKeyword);
 				keywords.add(lowerKeyword.substring(0, lowerKeyword.indexOf(":")));
 				keywords.add(lowerKeyword.substring(lowerKeyword.indexOf(":") + 1));
 			}
-			//split at ","
+			// split at ","
 			if (lowerKeyword.contains(",")) {
 				keywords.remove(lowerKeyword);
 				keywords.add(lowerKeyword.substring(0, lowerKeyword.indexOf(",")));
@@ -249,35 +276,35 @@ public class MediaTUMXMLDocument extends XMLDocument {
 	}
 
 	/**
-	 * select between two years a reasonable year, which is between 1500 and current+2
-	 * year has priority over facetYear if both are reasonable
-	 * if both are weird, say it and save it as 0, which will later be interpreted as NULL
+	 * Select between two years a reasonable year, which is between 1500 and current+2
+	 * year has priority over facetYear if both are reasonable.
+	 * If both are weird, say it and save it as 0, which will later be interpreted as NULL.
 	 */
 	private void selectYear() {
 		int tempYear = 0;
 		int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-		//if year is not reasonable (outside of 1500 until current+2
+		// if year is not reasonable (outside of 1500 until current+2
 		if (year < 1500 || year > currentYear + 2) {
-			//if facetyear also is weird
+			// if facetyear also is weird
 			if (facetYear < 1500 || facetYear > currentYear + 2) {
 				if (facetYear == 0 && year == 0) {
 				} else {
-					//say it and save as 0
+					// say it and save as 0
 					System.out.println("In Export " + documentPath + " In Document: " + id + "Weird Years: " + year
 							+ ", " + facetYear);
 					tempYear = 0;
 				}
-			//take facetYear if year is not reasonable but facetYear is
+			// take facetYear if year is not reasonable but facetYear is
 			} else
 				tempYear = facetYear;
-		//take year if year is reasonable
+		// take year if year is reasonable
 		} else
 			tempYear = year;
 		year = tempYear;
 	}
 
 	/**
-	 * set a clean title, dismissing every character which is not a character from a-z or a number. And put it in lower case
+	 * Set a clean title, dismissing every character which is not a character from a-z or a number. And put it in lower case.
 	 */
 	public String calculateTitleClean(String title) {
 		String temp;
@@ -287,15 +314,15 @@ public class MediaTUMXMLDocument extends XMLDocument {
 	}
 
 	/**
-	 * select a title from title and fulltitle (with priority on fulltitle)
+	 * Select a title from title and fulltitle (with priority on fulltitle).
 	 */
 	private void selectTitle() {
-		//if fulltitle is null, stay with title
+		// if fulltitle is null, stay with title
 		if (fulltitle == null) {}
-		//if fulltitle is not null but and contains more information thatn title, set fulltitle
+		// if fulltitle is not null but and contains more information thatn title, set fulltitle
 		else if (title.equals(fulltitle) || title == null || fulltitle.length() > title.length())
 			title = fulltitle;
-		//if fulltitle is not equals title and not as big, say it and stay with title
+		// if fulltitle is not equals title and not as big, say it and stay with title
 		else if (!title.equals(fulltitle)) {
 			System.out.println("In Export " + documentPath + " In Document: " + id);
 			System.out.println("Different titles!");
@@ -306,7 +333,7 @@ public class MediaTUMXMLDocument extends XMLDocument {
 	}
 
 	/**
-	 * if accidently the clean up of the title ended up with dismissing more than half of the characters (eg kanjis) stay with the normal title as cleantitle
+	 * If accidently the clean up of the title ended up with dismissing more than half of the characters (eg kanjis) stay with the normal title as cleantitle.
 	 */
 	private void setCleanTitle() {
 		if (cleantitle.length() <= title.length() / 2)
@@ -314,7 +341,7 @@ public class MediaTUMXMLDocument extends XMLDocument {
 	}
 
 	/**
-	 * preprocess the information from the document to a int
+	 * Preprocess the information from the document to a int.
 	 * 
 	 * @param year as String
 	 * @return year as Int
@@ -322,15 +349,15 @@ public class MediaTUMXMLDocument extends XMLDocument {
 	private int makeYearInt(String year) {
 		Matcher m = Pattern.compile("\\d").matcher(year);
 		
-		//if year is empty, set 0
+		// if year is empty, set 0
 		if (year == null)
 			return 0;
-		//if year contains only numbers
+		// if year contains only numbers
 		else if (m.find()) {
-			//but less than 4, set 0
+			// but less than 4, set 0
 			if (year.substring(m.start()).length() < 4)
 				return 0;
-			//otherwise take the first 4 consecutive digits as year
+			// otherwise take the first 4 consecutive digits as year
 			else if (year.substring(m.start(), m.start() + 4).matches("[0-9][0-9][0-9][0-9]"))
 				year = year.substring(m.start(), m.start() + 4);
 			else
@@ -341,7 +368,7 @@ public class MediaTUMXMLDocument extends XMLDocument {
 	}
 
 	/**
-	 * maps a language string to a 2 char enum (from ISO standard)
+	 * Maps a language string to a 2 char enum (from ISO standard).
 	 * 
 	 * @param language as String
 	 * @return language as 2 char enum
@@ -349,19 +376,19 @@ public class MediaTUMXMLDocument extends XMLDocument {
 	private String setLanguageToStandard(String language) {
 		boolean validLanguage = true;
 		String tempLan;
-		//if the language is not undefined
+		// if the language is not undefined
 		if (!(language == null || language.equals("Keine Angabe") || language.equals("Multilingual"))) {
-			//get the ISO languages
+			// get the ISO languages
 			String[] languages = Locale.getISOLanguages();
 			validLanguage = false;
 
-			//look up the language in the language Map (which has its information from a xml config file)
+			// look up the language in the language Map (which has its information from a xml config file)
 			tempLan = languageMap.get(language.toLowerCase());
 			
 			if (!(tempLan == null))
 				language = tempLan;
 
-			//if it contains bracktes, cut them of
+			// if it contains brackets, cut them of
 			if (language.contains("(") && language.contains(")")) {
 				String substring = language.substring(language.indexOf("(") + 1, language.indexOf(")")).toLowerCase();
 				//if it has the right length, compare it to the ISO languages
@@ -376,7 +403,7 @@ public class MediaTUMXMLDocument extends XMLDocument {
 		} else
 			language = null;
 		if (!validLanguage) {
-			//if its not a valid Language print out the wrong language but write null
+			// if its not a valid Language print out the wrong language but write null
 			System.out.println("In Export " + documentPath + " In Document: " + id + ": Language needs to be defined: "
 					+ language);
 			language = null;
@@ -472,26 +499,6 @@ public class MediaTUMXMLDocument extends XMLDocument {
 		this.language = language;
 	}
 
-	/**
-	 * 
-	 * Constructor, which initialize the needed Maps for mapping and resolving types and language
-	 * Set some default values
-	 * 
-	 * @param typeMap
-	 * @param languageMap
-	 * @param typeResolveMap
-	 */
-	public MediaTUMXMLDocument(Map<String, String> typeMap, Map<String, String> languageMap,
-			Map<Tuple, String> typeResolveMap) {
-		super(typeMap, languageMap, typeResolveMap);
-		
-		this.typeMap = typeMap;
-		this.languageMap = languageMap;
-		this.typeResolveMap = typeResolveMap;
-		type = "unknown";
-		publishedInRank = 10;
-	}
-
 	public String getType() {
 		return type;
 	}
@@ -537,7 +544,7 @@ public class MediaTUMXMLDocument extends XMLDocument {
 	}
 
 	/**
-	 * Set publishedIn in a priority manner
+	 * Set publishedIn in a priority manner.
 	 * journal_full_txt_mv > journal_title_txt_mv > journal_short_txt_mv > Satit_str > Sseries_str_mv > publisher
 	 * 
 	 * @param publishedIn the "journal" where the (academic) document is published in
@@ -570,7 +577,8 @@ public class MediaTUMXMLDocument extends XMLDocument {
 	}
 
 	/**
-	 * set an abstract to a document with a valided ISO language
+	 * Set an abstract to a document with a validated ISO language.
+	 * 
 	 * @param abstract
 	 * @param language
 	 */
